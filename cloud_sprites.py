390d9179f1eaa5b300d3e61cb661452d380ef8a7
coffejo <coffejo@bc.edu>
2020-01-27 11:52:09 -0500

    Added some comments

--- cloud_sprites.py
+++ cloud_sprites.py
@@ -3,14 +3,18 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 import random
 
+# Constants
 GREEN = (0, 255, 0)
 NUMBER_OF_CLOUDS = 6
 CLOUD_WIDTH = 101
 CLOUD_HEIGHT = 50
 
+# Clouds are the simplist of the sprites
+# They have no collision and don't even really scale themselves properly
 class Cloud(pygame.sprite.Sprite):
 	""" Represents a cloud """
 	
+	# create the cloud with the given image, horizontal scale, and screen dimensions
 	def __init__(self, image, horiz_scale = 1.0, SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 720):
 		super().__init__()
 		self.h_scale = horiz_scale
@@ -19,7 +23,8 @@ class Cloud(pygame.sprite.Sprite):
 		
 		self.rect.x = SCREEN_WIDTH + 1
 		self.rect.y = random.randint(50, SCREEN_HEIGHT - 50 - self.image.get_height())
-		
+	
+	# move the pipe 3 scaled pixels to the left, kill it when it gets off screen
 	def update(self):
 		self.rect.x -= int(3 * self.h_scale)
 		if self.rect.right < 0:
--- stupid_idiot_bird.py
+++ stupid_idiot_bird.py
@@ -37,6 +37,8 @@ CLOUD_HEIGHT = 50
 ADDED_ROT_ANGLE = 3
 HOPPING_ANGLE = 45
 
+#Code for putting outlines around text
+# shamelessly stolen from stack overflow
 _circle_cache = {}
 def _circlepoints(r):
     r = int(round(r))
@@ -75,7 +77,9 @@ def render(text, font, gfcolor=WHITE, ocolor=BLACK, opx=2):
 
     surf.blit(textsurface, (opx, opx))
     return surf
+# end of code shamelessly stolen from stack overflow
 
+#Simple collision method to use hitboxes rather than the image rects
 def collided(sprite, other):
 	return sprite.hitbox.colliderect(other.hitbox)
 
@@ -83,6 +87,8 @@ class Game(object):
 	""" Represents an instance of the game """
 	
 	def __init__(self, horiz_scale = 1.0, verti_scale = 1.0, s_width = 1280, s_height = 720, show_fps = False, sound_on = True):
+		# Setting initial variables and loading images to be passed to infinitely generated sprites
+		# (Loading the image once then passing it looks messier, but vastly improves performance)
 		self.score = 0
 		self.blockFrames = 15
 		
@@ -145,6 +151,7 @@ class Game(object):
 		self.bird.moveTo(self.player_x, self.player_y)
 		self.all_sprites_list.add(self.bird)
 		
+		# retrieving high score
 		high_score_tracker = shelve.open('high_score.txt')
 		try:
 			self.high_score = high_score_tracker['high_score']
@@ -152,18 +159,23 @@ class Game(object):
 			self.high_score = 0
 			high_score_tracker['high_score'] = 0
 		high_score_tracker.close()
-		
+	
+	# set player's upward velocity to 15, reset bird's rotation to 45 degrees, play a sound if applicable
 	def hop(self):
 		self.player_velo_y = -15.0
 		self.bird.rot_center(HOPPING_ANGLE - self.player_rot_angle)
 		self.player_rot_angle = HOPPING_ANGLE
 		if self.sound_on:
 			self.hop_sound.play()
-		
+	
+	# self explanitory method name
 	def process_events(self):
+		# if game is over, subtract 1 from blockFrames (variable used to block input when game ends)
 		if self.game_over:
 			self.blockFrames-=1
+			
 		for event in pygame.event.get():
+			# if the game is quit, save the high score if needed then return false (ending the game)
 			if event.type == pygame.QUIT:
 				high_score_tracker = shelve.open('high_score.txt')
 				if self.high_score > high_score_tracker['high_score']:
@@ -171,29 +183,40 @@ class Game(object):
 				high_score_tracker.close()
 				return False
 			if event.type == pygame.KEYDOWN:
+				# if player pressed space or up arrow, hop
 				if (event.key == pygame.K_UP or event.key == pygame.K_SPACE) and not self.game_over:
 					if not self.paused:
 						self.hop()
 					if not self.first_input_recieved:
 						self.first_input_recieved = True
+				# if the game is over and r or space is pressed (and not blocked) or if escape is pressed at any point
 				elif ((event.key == pygame.K_r or (event.key == pygame.K_SPACE and self.blockFrames <= 0)) and self.game_over) or event.key == pygame.K_ESCAPE:
+					# update the high score
 					high_score_tracker = shelve.open('high_score.txt')
 					if self.high_score > high_score_tracker['high_score']:
 						high_score_tracker['high_score'] = self.high_score
 					high_score_tracker.close()
+					
+					# if the key was r or space, play again. Else return false and end the game.
 					if event.key == pygame.K_r or event.key == pygame.K_SPACE:
 						self.__init__(self.h_scale, self.v_scale, self.screen_width, self.screen_height, self.show_fps, self.sound_on)
 					else:
 						return False
+				# if F3 is pressed, show debug info
 				elif event.key == pygame.K_F3:
 					self.show_fps = not self.show_fps
+				# if p is pressed, toggle paused state
 				elif event.key == pygame.K_p and self.first_input_recieved and not self.game_over:
 					self.paused = not self.paused
 		
 		return True
-		
+	
+	# self explanitory method name
 	def run_logic(self):
+		# only run if not paused
 		if not self.paused:
+		
+			# increment timers, then add new pipes and clouds if needed.
 			self.pipe_timer += 1
 			self.cloud_timer += 1
 			
@@ -228,27 +251,30 @@ class Game(object):
 				cloud = cloud_sprites.Cloud(cloud_image, self.h_scale, self.screen_width, self.screen_height)
 				
 				self.clouds_list.add(cloud)
-				
+			
+			# set the player's new y positon according to the players vertical velocity
 			self.player_y += self.player_velo_y
 			
+			# apply gravity to the player if not at terminal velocity and not on the ground (increment their vertical velocity and adjust their angle)
 			if self.player_velo_y < self.gravity and not self.player_on_ground:
 				self.player_velo_y += 1
 				self.bird.rot_center(-ADDED_ROT_ANGLE)
 				self.player_rot_angle -= ADDED_ROT_ANGLE % 360
 			
-			
+			# keep the player from going off the top of the screen, kill the player if they go off the bottom of the screen
 			if self.player_y < 0:
 				self.player_y = 0
 			elif int(self.player_y * self.v_scale) > self.screen_height:
 				self.game_over = True
 			
+			# update all sprites
 			if not self.game_over:
 				self.all_sprites_list.update()
 				self.clouds_list.update()
 				self.scorezone_list.update()
 			
+			# detect if player hit a scorezone, manipulate score and gap between pipes if needed
 			score_hit_list = pygame.sprite.spritecollide(self.bird, self.scorezone_list, True, collided)
-			
 			for zone in score_hit_list:
 				if not self.game_over:
 					self.score += 1
@@ -257,10 +283,16 @@ class Game(object):
 					elif self.score % 10 == 0 and self.pipe_gap > 60:
 						self.pipe_gap -= 1
 			
+			# detect if player hit a pipe, end the game and manipulate the bird's position according to the type of collision
 			self.player_on_ground = False
-			
 			pipe_hit_list = pygame.sprite.spritecollide(self.bird, self.pipes_list, False, collided)
 			for pipe in pipe_hit_list:
+				if not self.game_over:
+					if self.sound_on:
+						self.hit_sound.play()
+					self.game_over = True
+				
+				# get pipe's position and calculate if the player is between a top pipe and a bottom pipe
 				pipe_x_pos = pipe.hitbox.left/self.h_scale
 				pipe_y_pos = 0
 				
@@ -276,10 +308,7 @@ class Game(object):
 				if isinstance(pipe, pipe_sprites.Bottom_pipe) and self.player_y <= pipe_y_pos and self.player_y >= pipe_y_pos - self.pipe_gap + 100 and self.bird.hitbox.right >= pipe_x_pos:
 					player_between_pipes = True
 				
-				if not self.game_over:
-					if self.sound_on:
-						self.hit_sound.play()
-					self.game_over = True
+				# if the player landed on top of a bottom pipe, reset vertical velocity, push bird out of the pipe, and roll off the side of it
 				if self.player_velo_y > 0 and isinstance(pipe, pipe_sprites.Bottom_pipe) and player_between_pipes:
 					self.player_velo_y = 0
 					self.player_y = int(pipe.hitbox.top / self.v_scale) - ((((BIRD_HITBOX_HEIGHT + BIRD_HITBOX_WIDTH)/2)/2) - 1)
@@ -287,9 +316,12 @@ class Game(object):
 					self.bird.rot_center(-ADDED_ROT_ANGLE)
 					self.player_rot_angle -= ADDED_ROT_ANGLE % 360
 					self.player_on_ground = True
+				# if the bird hit the bottom of a top pipe, reset vertical velocity and push bird out of the pipe
 				elif self.player_velo_y < 0 and isinstance(pipe, pipe_sprites.Top_pipe) and player_between_pipes:
 					self.player_velo_y = 0
 					self.player_y = int((pipe.hitbox.bottom + (.5 * BIRD_HITBOX_HEIGHT)) / self.v_scale) + 1
+				# if bird hit the side of any pipe, push the bird out of the pipe and let gravity do its job
+				# fun fact: I had to actually go to one of my professors to figure out how to make it look like the bird wasn't hitting a god damn forcefield
 				elif not self.player_on_ground and not player_between_pipes:
 					pos_horiz_offset = (math.cos(math.radians(self.player_rot_angle)) * (.5 * BIRD_HITBOX_WIDTH)) - (math.sin(math.radians(self.player_rot_angle)) * (.5 * BIRD_HITBOX_HEIGHT))
 					neg_horiz_offset = (math.cos(math.radians(self.player_rot_angle)) * (.5 * BIRD_HITBOX_WIDTH)) - (math.sin(math.radians(self.player_rot_angle)) * (.5 * (-1 * BIRD_HITBOX_HEIGHT)))
@@ -298,38 +330,48 @@ class Game(object):
 					else:
 						self.player_x = pipe_x_pos - neg_horiz_offset 
 					
-				
+			# move the bird to its new position	
 			self.bird.moveTo(int(self.player_x * self.h_scale), int(self.player_y * self.v_scale))
 			
-			if self.game_over:
+			# outdated code
+			'''if self.game_over:
 				for pipe in self.pipes_list:
-					pipe.setMoving(False)
+					pipe.setMoving(False)'''
 			
+			# if no input has been given, keep the bird hopping around the middle of the screen
 			if not self.first_input_recieved and int(self.player_y * self.v_scale) > (self.screen_height / 2) + (.5 * BIRD_HEIGHT):
 				self.hop()
 				
+			# if a new high score has been achieved, update the high score variable and make a note of it for later
 			if self.score > self.high_score:
 				self.high_score = self.score
 				self.new_high_score = True
-		
+	
+	# sets the time at the start of the frame for purposes of fps tracking
 	def set_start_time(self, time):
 		self.start_time = time
 	
+	# sets the fps value to be displayed, calculated outside this class in the main method
 	def set_fps(self, frames):
 		self.fps = frames
 	
+	# self explanitory method name
 	def display_frame(self, screen):
+		# draw the sky
 		pygame.draw.rect(screen, SKY, pygame.Rect(0, 0, self.screen_width, self.screen_height))
 		
+		# draw the clouds then everything else
 		self.clouds_list.draw(screen)
 		self.all_sprites_list.draw(screen)
 		
+		# if the game isn't over, display the score and high score in the corner of the screen
 		if not self.game_over:
 			font = pygame.font.SysFont("Calibri", 25, True, False)
 			screen.blit(render('Score: ' + str(self.score), font), [10, 10])
 			if not self.high_score == 0:
 				screen.blit(render('High Score: ' + str(self.high_score), font), [10, 40])
 		
+		# if the player hasn't pressed anything yet, display the tooltip telling them how to hop
 		if not self.first_input_recieved:
 			font = pygame.font.SysFont("Calibri", 30, True, False)
 			text = font.render("press SPACE to hop", True, WHITE)
@@ -337,6 +379,7 @@ class Game(object):
 			y = 500 * self.v_scale
 			screen.blit(render("press SPACE to hop", font), [center_x, y])
 		
+		# if the game is over, display the end screen text
 		if self.game_over:
 			font = pygame.font.SysFont("Calibri", 40, True, False)
 			
@@ -369,47 +412,56 @@ class Game(object):
 				text = font.render("GAME OVER", True, WHITE)
 				center_y -= text.get_height() * 1.5
 				center_x = (self.screen_width // 2) - (text.get_width() // 2)
-				screen.blit(render("GAME OVER", font), [center_x, center_y])		
-
+				screen.blit(render("GAME OVER", font), [center_x, center_y])
+				
+		# if the game is paused, display PAUSED in the middle of the screen
 		if self.paused:
 			font = pygame.font.SysFont("Calibri", 40, True, False)
 			text = font.render("PAUSED", True, WHITE)
 			center_x = (self.screen_width // 2) - (text.get_width() // 2)
 			center_y = (self.screen_height // 2) - (text.get_height() // 2)
 			screen.blit(render("PAUSED", font), [center_x, center_y])
-			
+		
+		# if debug info should be shown, show it in the corner of the screen
 		if self.show_fps:
 				font = pygame.font.SysFont("Calibri", 25, True, False)
 				screen.blit(render(('FPS: ' + str(self.fps)), font), [self.screen_width - 150, 10])
 				screen.blit(render(('pyAngle: ' + str(self.player_rot_angle)), font), [self.screen_width - 150, 50])
 		
+		# flip the display (i.e. actually draw everything rather than just thinking about drawing it)
 		pygame.display.flip()
-		
+
+# Main method, execution starts here		
 def main():
+	# set up some initial stuff to make the sound work, then initiate pygame
 	pygame.mixer.pre_init(44100, -16, 2, 1024)
 	pygame.mixer.init()
 	pygame.init()
 	
+	# getting screen size and setting the game to fullscreen
 	ctypes.windll.user32.SetProcessDPIAware()
 	true_res = (ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1))
 	screen = pygame.display.set_mode(true_res,pygame.FULLSCREEN)
 	screen_width, screen_height = pygame.display.get_surface().get_size()
 	
+	# getting the amount to scale everything by for proper display
 	horizontal_scale = screen_width / 1280
 	vertical_scale = screen_height / 720
 	
+	# doesn't do anything anymore unless you somehow get the game into windowed mode
 	icon = pygame.image.load("resources/icon.png")
 	icon.set_colorkey(WHITE)
-	
 	pygame.display.set_icon(icon)
 	pygame.display.set_caption("Stupid Idiot Bird Can't Fly")
 	
+	# load and set the positition of the title image
 	title_image = pygame.image.load("resources/title.png").convert()
 	title_image = pygame.transform.scale(title_image, (int(TITLE_WIDTH * horizontal_scale), int(TITLE_HEIGHT * vertical_scale)))
 	title_image.set_colorkey(WHITE)
 	title_x = screen_width // 2 - int(TITLE_WIDTH * horizontal_scale / 2)
 	title_y = screen_height // 5
 	
+	# setting up button images, positions, and hitboxes
 	play_button_image = pygame.image.load("resources/play_button.png").convert()
 	play_button_image = pygame.transform.scale(play_button_image, (int(BUTTON_WIDTH * horizontal_scale), int(BUTTON_HEIGHT * vertical_scale)))
 	play_button_image.set_colorkey(WHITE)
@@ -443,6 +495,7 @@ def main():
 	sound_button_y1 = screen_height - (20 + int(11 * vertical_scale))
 	sound_button_y2 = screen_height - 20
 	
+	# setting up other inital variables
 	click_sound = pygame.mixer.Sound("resources/click.ogg")
 	
 	done = False
@@ -453,44 +506,55 @@ def main():
 	intro = True
 	playing = True
 	
+	# while the program is running
 	while not done:
 		pygame.mouse.set_visible(True)
+		# while we're on the title screen
 		while intro:
+			# detect mouse clicks and check if they're on any of our buttons
 			for event in pygame.event.get():
+				# if they closed the game, exit all loops (which basically exits the game)
 				if event.type == pygame.QUIT:
 					intro = False
 					playing = False
 					done = True
 				elif event.type == pygame.MOUSEBUTTONDOWN:
+					# if they left-clicked
 					if event.button == 1:
+						# if the mouse was over the play button, turn off the menu and turn on the game
 						if event.pos[0] in range(play_button_x_1, play_button_x_2) and event.pos[1] in range(play_button_y_1, play_button_y_2):
 							if sound_on:
 								click_sound.play()
 							intro = False
 							playing = True
+						# if the mouse was over the quit button, exit the game
 						if event.pos[0] in range(quit_button_x_1, quit_button_x_2) and event.pos[1] in range(quit_button_y_1, quit_button_y_2):
 							if sound_on:
 								click_sound.play()
 							intro = False
 							playing = False
 							done = True
+						# if the mouse was over the sound toggle button, toggle the sound (WOW!)
 						if event.pos[0] in range(sound_button_x1, sound_button_x2) and event.pos[1] in range(sound_button_y1, sound_button_y2):
 							sound_on = not sound_on
 							if sound_on:
 								click_sound.play()
+								
+				# if there was no mouse click and it was in fact a keystroke on the key ESC, then exit the game
 				elif event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_ESCAPE:
 						intro = False
 						playing = False
 						done = True
 						
-						
+			# get the mouse position for reasons unrelated to clicking			
 			mouse = pygame.mouse.get_pos()
 			
+			# draw everything
 			pygame.draw.rect(screen, SKY, pygame.Rect(0, 0, screen_width, screen_height))
-			
 			screen.blit(title_image, [title_x, title_y])
 			
+			# when drawing the buttons, if the mouse is over the button then display the lighter colored variant of it
 			if mouse[0] in range(play_button_x_1, play_button_x_2) and mouse[1] in range(play_button_y_1, play_button_y_2):
 				screen.blit(play_button_hover_image, [play_button_x_1, play_button_y_1])
 			else:
@@ -501,20 +565,26 @@ def main():
 			else:
 				screen.blit(quit_button_image, [quit_button_x_1, quit_button_y_1])
 			
+			# display the correct version of the sound toggle button, which doubles as an indicator of whether or not sound is on
 			if sound_on:
 				screen.blit(sound_on_image, [sound_button_x1, sound_button_y1])
 			else:
 				screen.blit(sound_off_image, [sound_button_x1, sound_button_y1])
-				
+						
 			pygame.display.flip()
 		
+		# upon exiting the main menu loop, we immediately set the variable determining if we should be in the main menu loop to true again
+		# so that in the future, when we leave the game we go back to the menu
 		intro = True
+		
+		# hide the mouse, initiate the start_time variable, and start up a new game 
 		pygame.mouse.set_visible(False)
 		
 		start_time = 0
 		
 		game = Game(horizontal_scale, vertical_scale, screen_width, screen_height, False, sound_on)
 		
+		# while playing, update the framerate if neede, process events, run logic, display a frame, then tick the clock
 		while playing:
 			update_fps = False
 			if game.show_fps and time.time() - game.start_time >= .25:
@@ -527,11 +597,12 @@ def main():
 			
 			game.display_frame(screen)
 			
-			clock.tick(60)
+			clock.tick(60) # this keeps us locked to a max of 60 fps
 			
 			if game.show_fps and update_fps:
 				game.set_fps(math.floor(1.0 / (time.time() - game.start_time)))
 	
+	# when all that is done, quit the game
 	pygame.quit()
 	
 if __name__ == "__main__":
